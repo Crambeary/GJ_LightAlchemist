64828b0d65c51a4afd94cafed5eb0c3515fe4aa8
Marc <marctotaro94@gmail.com>
2024-07-29 15:08:51 -0500

    enemy melee sprite for idle, run, and hit

--- addons/scripts/enemy_melee.gd
+++ addons/scripts/enemy_melee.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var sword_direction: Node2D = $"sword direction"
 @onready var animation_player: AnimationPlayer = $"sword direction/AnimationPlayer"
 @onready var flash_hit: AnimationPlayer = $FlashHit
+@onready var animated_sprite_2d: AnimatedSprite2D = $CollisionShape2D/AnimatedSprite2D
 
 
 @export var max_distance := 200
@@ -12,10 +13,8 @@ extends CharacterBody2D
 @export var melee_distance := 60
 
 var health := max_health
+var in_attack := false
 const SPEED = 50.0
-
-func getPlayer():
-	return Player
 
 
 func _physics_process(delta: float) -> void:
@@ -24,6 +23,7 @@ func _physics_process(delta: float) -> void:
 	var EnemyToPlayer = Player.position - position
 	if EnemyToPlayer.length() < max_distance and EnemyToPlayer.length() > min_distance:
 		# We move on to the next step.
+		sword_direction.look_at(Player.global_position)
 		direction = EnemyToPlayer.normalized()
 	
 	if EnemyToPlayer.length() < melee_distance && melee_cooldown.is_stopped():
@@ -31,9 +31,20 @@ func _physics_process(delta: float) -> void:
 	
 	# TODO: Is there a pathfinding package I can use?
 	velocity = direction * SPEED
+		
+	if direction and not in_attack:
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+			
+	if direction.x > 0:
+		# Determine which direction we are facing. Use Flip H And offset -1 for left facing
+		animated_sprite_2d.flip_h = false
+	elif direction.x < 0:
+		animated_sprite_2d.flip_h = true
 
 	move_and_slide()
-	sword_direction.look_at(Player.global_position)
+
 
 func take_damage(amount: int) -> void:
 	health = health - amount
@@ -46,17 +57,17 @@ func melee() -> void:
 	# We only execute this when in range, but maybe it's worth confirming for safety?
 	melee_cooldown.start()
 	animation_player.play("swing")
+	animated_sprite_2d.play("hit")
+	in_attack = true
 	
 	
-	
-
-
-func _on_sword_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
-
 func _on_sword_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		var body = area.get_parent()
-		print("hit")
 		body.take_damage(2)
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	# if anim_name == "hit":
+	if animated_sprite_2d.animation == "hit":
+		in_attack = false
--- addons/scripts/player.gd
+++ addons/scripts/player.gd
@@ -22,78 +22,78 @@ var shoot_on_cooldown := false
 var health := max_health
 
 func _physics_process(delta: float) -> void:
-    # Get the input direction and handle the movement/deceleration.
-    # As good practice, you should replace UI actions with custom gameplay actions.
-    var direction := Input.get_vector("left", "right", "up", "down")
-    
-    if not isTeleporting:
-        if direction.x > 0:
-            # Determine which direction we are facing. Use Flip H And offset -1 for left facing
-            animated_sprite_2d.offset.x = 1
-            animated_sprite_2d.flip_h = false
-            walking_dust.offset.x = 0
-            walking_dust.flip_h = false
-        elif direction.x < 0:
-            animated_sprite_2d.offset.x = -1
-            animated_sprite_2d.flip_h = true
-            walking_dust.offset.x = 18
-            walking_dust.flip_h = true
-            
-        if direction:
-            animated_sprite_2d.play("Run")
-            walking_dust.play("default")
-            walking_dust.show()
-        else:
-            animated_sprite_2d.play("idle")
-            walking_dust.hide()
-            walking_dust.stop()
-        
-        
-        velocity = direction * SPEED
-        
-    move_and_slide()
-    
-    if Input.is_action_just_pressed("shoot"):
-        shoot()
-        
-    if Input.is_action_just_pressed("teleport") && direction:
-        isTeleporting = true
-        animated_sprite_2d.play("invisible")
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_vector("left", "right", "up", "down")
+	
+	if not isTeleporting:
+		if direction.x > 0:
+			# Determine which direction we are facing. Use Flip H And offset -1 for left facing
+			animated_sprite_2d.offset.x = 1
+			animated_sprite_2d.flip_h = false
+			walking_dust.offset.x = 0
+			walking_dust.flip_h = false
+		elif direction.x < 0:
+			animated_sprite_2d.offset.x = -1
+			animated_sprite_2d.flip_h = true
+			walking_dust.offset.x = 18
+			walking_dust.flip_h = true
+			
+		if direction:
+			animated_sprite_2d.play("Run")
+			walking_dust.play("default")
+			walking_dust.show()
+		else:
+			animated_sprite_2d.play("idle")
+			walking_dust.hide()
+			walking_dust.stop()
+		
+		
+		velocity = direction * SPEED
+		
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+		
+	if Input.is_action_just_pressed("teleport") && direction:
+		isTeleporting = true
+		animated_sprite_2d.play("invisible")
 
-        
-    if isTeleporting:
-        var _invisible_duration := animated_sprite_2d.sprite_frames.get_frame_duration("invisible", 0) 
-        var _invisible_length := animated_sprite_2d.sprite_frames.get_frame_count("invisible")
-        var _invisible_speed := animated_sprite_2d.sprite_frames.get_animation_speed("invisible")
-        var _invisible_time := _invisible_duration * _invisible_length / _invisible_speed
-        velocity = Vector2(0,0)
-    
-        if is_teleporting_time > 0:
-            is_teleporting_time -= delta
-        else:
-            isTeleporting = false
-            is_teleporting_time = teleport_time
-        
-        if is_teleporting_time < _invisible_time:
-            translate(direction * (SPEED * delta * 1.8))
-            animated_sprite_2d.play_backwards("invisible")
-    
+		
+	if isTeleporting:
+		var _invisible_duration := animated_sprite_2d.sprite_frames.get_frame_duration("invisible", 0) 
+		var _invisible_length := animated_sprite_2d.sprite_frames.get_frame_count("invisible")
+		var _invisible_speed := animated_sprite_2d.sprite_frames.get_animation_speed("invisible")
+		var _invisible_time := _invisible_duration * _invisible_length / _invisible_speed
+		velocity = Vector2(0,0)
+	
+		if is_teleporting_time > 0:
+			is_teleporting_time -= delta
+		else:
+			isTeleporting = false
+			is_teleporting_time = teleport_time
+		
+		if is_teleporting_time < _invisible_time:
+			translate(direction * (SPEED * delta * 1.8))
+			animated_sprite_2d.play_backwards("invisible")
+	
 func shoot():
-    if not shoot_on_cooldown:
-        var b = Projectile.instantiate()
-        owner.add_child(b)
-        b.transform = $Hand/CollisionShape2D/AnimatedSprite2D/Marker2D.global_transform
-        hand_animation.play("bloom")
-        shoot_on_cooldown = true
+	if not shoot_on_cooldown:
+		var b = Projectile.instantiate()
+		owner.add_child(b)
+		b.transform = $Hand/CollisionShape2D/AnimatedSprite2D/Marker2D.global_transform
+		hand_animation.play("bloom")
+		shoot_on_cooldown = true
 
 func take_damage(amount: int) -> void:
-    $SFX_Hurt.play()
-    health = health - amount
-    animated_sprite_2d.modulate.r = 255 /(health - max_health)
-    flash_hit.play("hit")
-    if health <= 0:
-        health_depleated.emit()
+	$SFX_Hurt.play()
+	health = health - amount
+	animated_sprite_2d.modulate.r = 255 /(health - max_health)
+	flash_hit.play("hit")
+	if health <= 0:
+		health_depleated.emit()
 
 func _on_hand_animation_finished() -> void:
-    hand_animation.play("idle")
-    shoot_on_cooldown = false
+	hand_animation.play("idle")
+	shoot_on_cooldown = false
