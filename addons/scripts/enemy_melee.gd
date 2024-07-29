e5fe6ed8966e72ee39c079387fde488c46bc9460
Marc <marctotaro94@gmail.com>
2024-07-29 17:49:26 -0500

    enemy death animation
    collision disabled on death. Dead body stays in place

--- addons/scripts/enemy_melee.gd
+++ addons/scripts/enemy_melee.gd
@@ -16,42 +16,50 @@ extends CharacterBody2D
 var health := max_health
 var in_attack := false
 const SPEED = 50.0
+var is_dead := false
 
 
 func _physics_process(delta: float) -> void:
-    var direction := Vector2(0,0)
-    # Get player location relative to me
-    var EnemyToPlayer = Player.position - position
-    if EnemyToPlayer.length() < max_distance and EnemyToPlayer.length() > min_distance:
-        # We move on to the next step.
-        sword_direction.look_at(Player.global_position)
-        direction = EnemyToPlayer.normalized()
-    
-    if EnemyToPlayer.length() < melee_distance && melee_cooldown.is_stopped():
-        melee()
-    
-    # TODO: Is there a pathfinding package I can use?
-    velocity = direction * SPEED
+    if not is_dead:
+        var direction := Vector2(0,0)
+        # Get player location relative to me
+        var EnemyToPlayer = Player.position - position
+        if EnemyToPlayer.length() < max_distance and EnemyToPlayer.length() > min_distance:
+            # We move on to the next step.
+            sword_direction.look_at(Player.global_position)
+            direction = EnemyToPlayer.normalized()
         
-    if direction and not in_attack:
-        animated_sprite_2d.play("run")
-    else:
-        animated_sprite_2d.play("idle")
+        if EnemyToPlayer.length() < melee_distance && melee_cooldown.is_stopped():
+            melee()
+        
+        # TODO: Is there a pathfinding package I can use?
+        velocity = direction * SPEED
             
-    if direction.x > 0:
-        # Determine which direction we are facing. Use Flip H And offset -1 for left facing
-        animated_sprite_2d.flip_h = false
-    elif direction.x < 0:
-        animated_sprite_2d.flip_h = true
+        if direction and not in_attack:
+            animated_sprite_2d.play("run")
+        else:
+            animated_sprite_2d.play("idle")
+                
+        if direction.x > 0:
+            # Determine which direction we are facing. Use Flip H And offset -1 for left facing
+            animated_sprite_2d.flip_h = false
+        elif direction.x < 0:
+            animated_sprite_2d.flip_h = true
 
-    move_and_slide()
-
+        move_and_slide()
+        
+func die() -> void:
+    animated_sprite_2d.play("death")
+    $"sword direction".hide()
+    $"sword direction/Sword/CollisionShape2D2".disabled = true
+    is_dead = true
+    set_collision_layer_value(1, false)
 
 func take_damage(amount: int) -> void:
     health = health - amount
     flash_hit.play("hit")
     if health <= 0:
-        queue_free()
+        die()
 
 func melee() -> void:
     # We only execute this when in range, but maybe it's worth confirming for safety?
