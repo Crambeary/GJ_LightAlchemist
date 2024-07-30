0afab6c1a7914d9089f2fc0a51fabf4db0f54377
Marc <marctotaro94@gmail.com>
2024-07-30 18:21:27 -0500

    added itch coverpage
    also balanced music

--- addons/scripts/player.gd
+++ addons/scripts/player.gd
@@ -27,6 +27,7 @@ func _physics_process(delta: float) -> void:
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
     var direction := Input.get_vector("left", "right", "up", "down")
+    #var direction := Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
     
     if not isTeleporting:
         if direction.x > 0:
