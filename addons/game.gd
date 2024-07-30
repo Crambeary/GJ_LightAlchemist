3bd93d1dac2a93cbf3fc46dfbe79befa0b2853a1
Marc <marctotaro94@gmail.com>
2024-07-30 16:09:00 -0500

    checkpoints

--- addons/game.gd
+++ addons/game.gd
@@ -1,4 +1,9 @@
 extends Node2D
+@onready var player: CharacterBody2D = %Player
+
+func _ready() -> void:
+    Events.checkpoint_entered.connect(func(checkpoint):
+        $PlayerSpawn.global_position = checkpoint.global_position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -11,7 +16,9 @@ func _process(delta: float) -> void:
 
 
 func game_over() -> void:
-    get_tree().reload_current_scene()
+    #get_tree().reload_current_scene()
+    player.position = $PlayerSpawn.position
+    player.heal(8)
 
 
 func _on_player_health_depleated() -> void:
